98baf1400bc0d07d8da1dc01a83c123255926584
lcreina <backtrunck@yahoo.com.br>
2023-03-16 14:32:52 -0300

    inclusão da paginação em pesquisar_produtos

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
 import pickle
 
-from flask import Flask, render_template, request, redirect, session, url_for
+from flask import Flask, render_template, request, redirect, session, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, StringField, SubmitField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
-# criar um modelo para categoria, criar um form para categoria e um template para pesquisar categoria.
-# utilizar o modelo, o form, o template e a rota de produto como exemplo.
+PAGES = 3
 
 db = SQLAlchemy()
 app = Flask(__name__)
@@ -42,13 +41,35 @@ class CustomFlaskForm(FlaskForm):
             if class_attribute:
                 class_attribute.data = value
 
+class CategoriaForm(CustomFlaskForm):
+    id = StringField('id')
+    ds_categoria = StringField('Descrição')
+    submit = SubmitField('Pesquisar')
 
 class ProdutoForm(CustomFlaskForm):
     id = StringField('id')
-    cod_barras = StringField('Cód. Barras', validators=[DataRequired()])
+    # validators=[DataRequired()]
+    cod_barras = StringField('Cód. Barras')
     ds_produto = StringField('Descrição')
     categoria_id = StringField('Categoria')
     submit = SubmitField('Pesquisar')
+
+
+class Categoria(db.Model):
+    __tablename__ = 'categoria'
+    id = db.Column(db.Integer, primary_key=True)
+    ds_categoria = db.Column(db.String(200))\
+
+
+    @classmethod
+    def buscar_categoria(cls, parametros):
+        query = cls.query.order_by(cls.ds_categoria)
+        for field_key, field_value in parametros.items():
+            if field_key == 'id':
+                query = query.filter(cls.id == field_value)
+            elif field_key == 'ds_categoria':
+                query = query.filter(cls.ds_categoria.like(f'%{field_value}%'))
+        return query
 
 
 class Cliente(db.Model):
@@ -104,9 +125,9 @@ def index():
 
     return render_template('base.html')
 
-
-@app.route('/produtos', methods=['GET', 'POST'])
-def obter_produtos():
+@app.route('/produtos', defaults={'page': 1}, methods=('post', 'get'))
+@app.route('/produtos/<int:page>')
+def obter_produtos(page):
     param_pesquisa = 'param_pesquisa'
     pagina_atual = 'obter_produtos'
     form_produto = ProdutoForm()
@@ -137,19 +158,76 @@ def obter_produtos():
         if campos_preenchidos:
             # o dict não é vazio
             query = Produto.buscar_produto(campos_preenchidos)
-            produtos = query.all()
+            #query = query.all()
             form_produto.fill_form(campos_preenchidos)
         else:
-            # ainda não tem código de barras inserido na sessão
-            produtos = Produto.query.all()
+            query = Produto.query
+
+        paginacao = query.paginate(page=page, per_page=PAGES)
+        # a pesquisa retornou dados?
+        if paginacao.pages == 0:
+            flash('Não foram encontrados contratos para esta pesquisa.')
+            produtos = []
+        else:
+            # qt_itens = PAGES
+            # if paginacao.pages > 1:
+            #     qt_itens = PAGES * (paginacao.pages - 1)
+            produtos = paginacao.items
 
     return render_template('pesquisar_produtos.html',
                            header='Pesquisar Produtos',
                            dados=produtos,
-                           form=form_produto)
+                           form=form_produto,
+                           paginacao=paginacao)
 
 
-@app.route('/cliente/<id_cliente>')
+@app.route('/categoria', methods=['GET', 'POST'])
+def obter_categoria():
+    param_pesquisa = 'param_pesquisa'
+    pagina_atual = 'obter_categoria'
+    form_categoria = CategoriaForm()
+
+    if session.get('pagina_atual', None) != pagina_atual:
+        session.pop(param_pesquisa, None)
+        session['pagina_atual'] = pagina_atual
+
+    if form_categoria.validate_on_submit():
+
+        if form_categoria.submit.raw_data[0].upper() == 'FECHAR':
+            # apagando os dados de pesquisa q estão na sessão.
+            session.pop(param_pesquisa, None)
+            # indo para a página principal
+            return redirect(url_for('index'))  # é porque foi enviado um post
+
+        # pega os dados do formulario (is_any_filled) e guarda na sessão no "formato string pickles".
+        session[param_pesquisa] = pickle.dumps(form_categoria.is_any_filled())
+        return redirect(url_for('obter_categoria'))
+
+    else:
+        # pega da sessão a informação preenchida no formulário no "formato string pickles"
+        campos_preenchidos = session.get(param_pesquisa, None)
+        # se veio dados da sessão
+
+        if campos_preenchidos:
+            # pega a inforamação no "formato string pickles" e transforma num dicionário
+            campos_preenchidos = pickle.loads(campos_preenchidos)
+
+        if campos_preenchidos:
+            # o dict não é vazio
+            query = Categoria.buscar_categoria(campos_preenchidos)
+            categorias = query.all()
+            form_categoria.fill_form(campos_preenchidos)
+        else:
+            # ainda não tem código de barras inserido na sessão
+            categorias = Categoria.query.all()
+        return render_template('pesquisar_categoria.html',
+                                header='Pesquisar Categoria',
+                                dados=categorias,
+                                form=form_categoria)
+
+
+@app.route('/cliente/', defaults={'id_cliente': 1}, methods=('post', 'get'))
+@app.route('/cliente/<int:id_cliente>')
 def obter_cliente(id_cliente):
     pagina_atual = 'obter_cliente'
     # a requisição está vindo de uma página diferente?????
@@ -170,3 +248,6 @@ def obter_cliente(id_cliente):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
+
+
